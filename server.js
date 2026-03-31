61869735e7ab00327558434412cd4cb1de4d8c5c
Laura Delissen <lauradelissen@hotmail.com>
2026-03-31 14:55:54 +0200

    cors error tackled

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
